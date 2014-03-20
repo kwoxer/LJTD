13ccecde2ab4cdb77da5d1666eb4a0ab78d6a71b
kwoxer <kwoxer@web.de>
2014-03-20 23:49:10 +0100

    New Write2Chat method and code cleanup - replaced the Sendkeys with Postmessage what actually runs faster und is more reliable - cleaned the WindowsManagement code, deleted unused function, changed also some visibility

--- Jungle Timer/Module_WindowManagement.vb
+++ Jungle Timer/Module_WindowManagement.vb
@@ -1,15 +1,8 @@
 ﻿Module Module_WindowManagement
-    Public Const WM_NCLBUTTONDOWN As Integer = &HA1
-    Public Const HT_CAPTION As Integer = &H2
     Public Declare Function SendMessage Lib "user32.dll" Alias "SendMessageA" (ByVal hwnd As Int32, ByVal wMsg As Int32, ByVal wParam As Int32, ByVal lParam As Int32) As Int32
     Public Declare Function ReleaseCapture Lib "user32.dll" () As Int32
-    Public Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
-    Public Declare Function SetForegroundWindow Lib "user32.dll" (ByVal hwnd As IntPtr) As Int32
     Public tHandle As Long = FindWindow(vbNullString, "League of Legends (TM) Client")
-    Private Declare Function BringWindowToTop Lib "user32" (ByVal hwnd As Long) As Long
-    Private Declare Function SetActiveWindow Lib "user32.dll" (ByVal hWnd As Long) As Long
-    Private Declare Function apiShowWindow Lib "user32" Alias "ShowWindow" (ByVal hwnd As IntPtr, ByVal nCmdShow As Integer) As Integer
-    Private Const SW_NORMAL As Integer = 1
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
     Private resource As Resources = Resources.Resources
     Public Structure PointAPI
         Public x As Integer
@@ -19,9 +12,11 @@
     Public Declare Auto Function SetCursorPos Lib "User32.dll" (ByVal X As Integer, ByVal Y As Integer) As Long
     Declare Function GetCursorPos Lib "user32.dll" (ByRef lpPoint As PointAPI) As Boolean
     Private Const MOUSEEVENTF_LEFTDOWN = &H2
-    Const MOUSEEVENTF_LEFTUP As Integer = &H4
+    Private Const MOUSEEVENTF_LEFTUP As Integer = &H4
     Private Const MOUSEEVENTF_RIGHTDOWN = &H8
-    Const MOUSEEVENTF_RIGHTUP As Integer = &H10
+    Private Const MOUSEEVENTF_RIGHTUP As Integer = &H10
+    Private Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Private Const HT_CAPTION As Integer = &H2
     Private x, y As Integer
     Public Sub Position_Click(ByVal midScreen_x As Integer, ByVal midScreen_y As Integer)
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
@@ -30,7 +25,6 @@
         SetCursorPos(midScreen_x, midScreen_y)
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
-
     End Sub
     Public Sub SetBackupCursorPos()
         Dim pt As PointAPI
--- Jungle Timer/Module_Write2Chat.vb
+++ Jungle Timer/Module_Write2Chat.vb
@@ -1,77 +1,89 @@
 ﻿Imports System.Runtime.InteropServices
+Imports System.Text
 Module Module_Write2Chat
-    Private resource As Resources = Resources.Resources
-    Private keyCtrl As String = "^ "
-    Private keyAlt As String = "% "
-    Private keyEnter As String = "~"
-    Private keyTab As String = "{TAB}"
     Private running As Boolean = False
+    Private Declare Function PostMessage Lib "user32.dll" Alias "PostMessageA" (ByVal hwnd As Long, ByVal wMsg As Long, ByVal wParam As Long, ByVal lParam As Long) As Long
+
     Public Sub Write2Chat_Send(text As String, modeRememberTimes As Boolean)
-        If Not running Then
-            running = True
-            If tHandle <> 0 Then
-                LJTD.Hook_Disable()
-                Try
-                    
-                    'Module_WindowManagement.Foreground_Set()
-                    Threading.Thread.Sleep(resource.PropDelayInt(0, 1))
-                    If LJTD.OpenerValue_Get <> "CTRL" Then
-                        SendKeys.SendWait(keyEnter)
-                    End If
-                    Threading.Thread.Sleep(resource.PropDelayInt(1, 1))
-                    If LJTD.OpenerValue_Get = "NONE" Then
-                        SendKeys.SendWait(text)
-                    End If
-                    If LJTD.OpenerValue_Get = "CTRL" Then
-                        'SendKeys.SendWait(keyCtrl + keyEnter + text)
-                        'SendKeys.SendWait(keyCtrl + text)
-                        'SendKeys.SendWait(keyEnter + text)
-                        'SendKeys.SendWait(keyEnter + keyCtrl + text)
-                        'SendKeys.SendWait(text)
-                        'SendKeys.SendWait(keyCtrl + keyEnter + text + keyEnter)
-                        'SendKeys.SendWait(keyAlt + keyEnter + text)
-                        'SendKeys.SendWait(keyAlt + text)
-                        'SendKeys.SendWait(keyEnter + "^ asdasd" + keyEnter)
-                        'SendKeys.SendWait(keyEnter + "asdasd" + keyEnter)
-                        'SendKeys.SendWait("^" + keyAlt + text)
-                        'SendKeys.SendWait("^" + keyEnter + keyEnter + text)
-                        'SendKeys.SendWait(keyAlt + keyEnter + text + keyEnter)
-                        'SendKeys.SendWait("%dasd")
-                        'SendKeys.SendWait(keyEnter)
-                        'SendKeys.SendWait(text)
-                    End If
-                    If LJTD.OpenerValue_Get = "ALT" Then
-                        If modeRememberTimes Then
-                            SendKeys.SendWait(keyAlt + text)
-                        Else
-                            If Module_Generate.WindowsVersion_Generate() = "Windows 7" Then
-                                SendKeys.SendWait(keyAlt + text)
-                            Else
-                                If LJTD.ButtonPressed Then
-                                    SendKeys.SendWait(keyAlt + text)
-                                Else
-                                    SendKeys.SendWait(keyAlt + keyEnter + text)
-                                End If       
-                            End If
-                        End If
-                    End If
-                    If LJTD.OpenerValue_Get = "SPACE" Then
-                        SendKeys.SendWait(keyCtrl + text)
-                    End If
-                    Threading.Thread.Sleep(resource.PropDelayInt(2, 1))
-                    If LJTD.OpenerValue_Get <> "CTRL" Then
-                        SendKeys.SendWait(keyEnter)
-                    End If
-                Catch ex As Exception
-                    'MsgBox(ex.Message & "Failure! " & tHandle)
-                    Module_Logfile.Logfile_Append("Write2Chat failed. Maybe your hotkey opener " + LJTD.OpenerValue_Get + "has not been correctly set.")
-                    running = False
-                End Try
-                LJTD.Hook_Enable()
-            Else
-                'MsgBox("Game not running maybe?!" & "Windows Handle " & tHandle)
-            End If
-            running = False
-        End If
+        Const WM_CHAR As Long = &H102
+        Const WM_KEYDOWN As Integer = &H100
+        Const WM_KEYUP As Integer = &H101
+        PostMessage(tHandle, WM_KEYDOWN, Keys.Enter, 1&)
+        PostMessage(tHandle, WM_KEYUP, Keys.Enter, 1&)
+        Threading.Thread.Sleep(10)
+        For i = 1 To text.Length
+            PostMessage(tHandle, WM_CHAR, Asc(Mid(text, i, 1)), 1&)
+        Next
+        Threading.Thread.Sleep(10)
+        PostMessage(tHandle, WM_KEYDOWN, Keys.Enter, &H1C0001)
+        PostMessage(tHandle, WM_KEYUP, Keys.Enter, &H1C0001)
+      
+
+        'If Not running Then
+        '    running = True
+        '    If Module_WindowManagement.tHandle <> 0 Then
+        '        LJTD.Hook_Disable()
+        '        Try
+
+        '            'Module_WindowManagement.Foreground_Set()
+        '            Threading.Thread.Sleep(resource.PropDelayInt(0, 1))
+        '            If LJTD.OpenerValue_Get <> "CTRL" Then
+        '                SendKeys.SendWait(keyEnter)
+        '            End If
+        '            Threading.Thread.Sleep(resource.PropDelayInt(1, 1))
+        '            If LJTD.OpenerValue_Get = "NONE" Then
+        '                SendKeys.SendWait(text)
+        '            End If
+        '            If LJTD.OpenerValue_Get = "CTRL" Then
+        '                'SendKeys.SendWait(keyCtrl + keyEnter + text)
+        '                'SendKeys.SendWait(keyCtrl + text)
+        '                'SendKeys.SendWait(keyEnter + text)
+        '                'SendKeys.SendWait(keyEnter + keyCtrl + text)
+        '                'SendKeys.SendWait(text)
+        '                'SendKeys.SendWait(keyCtrl + keyEnter + text + keyEnter)
+        '                'SendKeys.SendWait(keyAlt + keyEnter + text)
+        '                'SendKeys.SendWait(keyAlt + text)
+        '                'SendKeys.SendWait(keyEnter + "^ asdasd" + keyEnter)
+        '                'SendKeys.SendWait(keyEnter + "asdasd" + keyEnter)
+        '                'SendKeys.SendWait("^" + keyAlt + text)
+        '                'SendKeys.SendWait("^" + keyEnter + keyEnter + text)
+        '                'SendKeys.SendWait(keyAlt + keyEnter + text + keyEnter)
+        '                'SendKeys.SendWait("%dasd")
+        '                'SendKeys.SendWait(keyEnter)
+        '                'SendKeys.SendWait(text)
+        '            End If
+        '            If LJTD.OpenerValue_Get = "ALT" Then
+        '                If modeRememberTimes Then
+        '                    SendKeys.SendWait(keyAlt + text)
+        '                Else
+        '                    If Module_Generate.WindowsVersion_Generate() = "Windows 7" Then
+        '                        SendKeys.SendWait(keyAlt + text)
+        '                    Else
+        '                        If LJTD.ButtonPressed Then
+        '                            SendKeys.SendWait(keyAlt + text)
+        '                        Else
+        '                            SendKeys.SendWait(keyAlt + keyEnter + text)
+        '                        End If
+        '                    End If
+        '                End If
+        '            End If
+        '            If LJTD.OpenerValue_Get = "SPACE" Then
+        '                SendKeys.SendWait(keyCtrl + text)
+        '            End If
+        '            Threading.Thread.Sleep(resource.PropDelayInt(2, 1))
+        '            If LJTD.OpenerValue_Get <> "CTRL" Then
+        '                SendKeys.SendWait(keyEnter)
+        '            End If
+        '        Catch ex As Exception
+        '            'MsgBox(ex.Message & "Failure! " & tHandle)
+        '            Module_Logfile.Logfile_Append("Write2Chat failed. Maybe your hotkey opener " + LJTD.OpenerValue_Get + "has not been correctly set.")
+        '            running = False
+        '        End Try
+        '        LJTD.Hook_Enable()
+        '    Else
+        '        'MsgBox("Game not running maybe?!" & "Windows Handle " & tHandle)
+        '    End If
+        '    running = False
+        'End If
     End Sub
 End Module
